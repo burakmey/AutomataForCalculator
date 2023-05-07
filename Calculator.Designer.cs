c5241656d22fcd931d2cbf3e09430d0197fdd3e4
burak meydan <burak_meydan-gs@hotmail.com>
2023-05-07 09:46:22 +0300

    Calculator buttons added.

---
+++ Calculator.Designer.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace AutomataForCalculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.inputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.inputLabel.Location = new System.Drawing.Point(0, 0);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.inputLabel.Size = new System.Drawing.Size(345, 80);
+            this.inputLabel.TabIndex = 0;
+            this.inputLabel.Text = "0";
+            this.inputLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 440);
+            this.Controls.Add(this.inputLabel);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label inputLabel;
+    }
+}
---
+++ Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutomataForCalculator
+{
+    public partial class Calculator : Form
+    {
+        static readonly string[] buttonNames = { "buttonx", "buttony", "buttonClear", "buttonRemove",
+                                        "button7", "button8", "button9", "buttonDivision",
+                                        "button4", "button5", "button6", "buttonMultiplication",
+                                        "button1", "button2", "button3", "buttonSubtraction",
+                                        "buttonParenthesesL", "button0", "buttonParenthesesR", "buttonAddition",
+                                        "buttonSign", "buttonz", "buttonPoint", "buttonEqual" };
+        static readonly string[] buttonTexts = { "", "", "C", "<-",
+                                        "7", "8", "9", "/",
+                                        "4", "5", "6", "x",
+                                        "1", "2", "3", "-",
+                                        "(", "0", ")", "+",
+                                        "+/-", "", ".", "=" };
+        static int buttonCount = 24;
+        Point startingLocation = new Point(5, 110);
+        Size buttonSize = new Size(80, 50);
+        Button[] buttons = new Button[buttonCount];
+        public Calculator()
+        {
+            InitializeComponent();
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i] = new Button();
+                buttons[i].Name = buttonNames[i];
+                buttons[i].Text = buttonTexts[i];
+                buttons[i].Location = new Point(startingLocation.X + (i % 4) * (buttonSize.Width + startingLocation.X),
+                    startingLocation.Y + (i / 4) * (buttonSize.Height + startingLocation.X));
+                buttons[i].Size = buttonSize;
+                buttons[i].FlatStyle = FlatStyle.Flat;
+                buttons[i].FlatAppearance.BorderSize = 1;
+                buttons[i].FlatAppearance.BorderColor = SystemColors.ActiveBorder;
+                buttons[i].BackColor = SystemColors.ControlLightLight;
+                buttons[i].TabStop = false;
+                buttons[i].Click += new EventHandler(ButtonClick);
+                Controls.Add(buttons[i]);
+            }
+        }
+        void ButtonClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
